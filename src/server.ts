f1fbdb3a27041d8728158cd6e770b15ac694cdca
Scott Moss <willscottmoss@gmail.com>
2025-08-13 09:01:46 -0500

    solution diff

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,10 @@
-import { env, isDev, isTestEnv } from '../env.ts'
 import express from 'express'
-import cors from 'cors'
-import helmet from 'helmet'
 import authRoutes from './routes/authRoutes.ts'
 import habitRoutes from './routes/habitRoutes.ts'
 import userRoutes from './routes/userRoutes.ts'
-import morgan from 'morgan'
 
 const app = express()
 
-app.use(helmet())
-app.use(
-  cors({
-    origin: env.CORS_ORIGIN,
-    credentials: true,
-  })
-)
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(
-  morgan('dev', {
-    skip: () => isTestEnv(),
-  })
-)
 // Health check endpoint
 app.get('/health', (req, res) => {
   res.status(200).json({
