1f4693a5d1e8aec0f202655c57207502ebf54ffb
Scott Moss <willscottmoss@gmail.com>
2025-08-13 09:20:40 -0500

    solution diff

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { env } from '../env.ts'
-import app from './server.ts'
-
-app.listen(env.PORT, () => {
-  console.log(`Server running on port ${env.PORT}`)
-  console.log(`Environment: ${env.APP_STAGE}`)
-})
